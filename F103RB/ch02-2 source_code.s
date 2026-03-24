d2e6a240467f2aafc41cb50dbd12734a4c6f1087
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2026-03-24 12:03:57 +0900

    Add new source code file for chapter 2

---
+++ F103RB/ch02-2 source_code.s
@@ -0,0 +1,11 @@
+    EXPORT __main
+    AREA PROG, CODE, READONLY
+__main
+    MOV		R0,#0
+    MSR 	APSR,R0 ;clear APSR
+    MOVS    R2,#1   ; R1 = 1
+    MOVS    R3,#-1  ; R2 = 0xFFFFFFFF
+    ADDS    R1,R2,R3    ; R1 = R2+R3
+    NOP
+L4  B   L4 ; stay here forever
+    END
